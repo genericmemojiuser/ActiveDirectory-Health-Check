ffe098603235840dd3410c3d91aa295bbf1193d8
genericmemojiuser <stones1233@gmail.com>
2023-12-31 17:32:34 -0600

    Edit URI variable for required parameter

--- AD_Health_Check/RSAT_setup.ps1
+++ AD_Health_Check/RSAT_setup.ps1
@@ -1,7 +1,7 @@
 # Download RSAT from microsoft.com
 
 $parameters = @{
-    URI = "https://download.microsoft.com/download/1/"
+    URI = "https://download.microsoft.com/download/1/D/8/1D8B5022-5477-4B9A-8104-6A71FF9D98AB/WindowsTH-KB2693643-x64.msu"
     Outfile = "C:\WindowsTH-KB2693643-x64.msu"
 }
 Invoke-WebRequest @parameters
@@ -14,3 +14,4 @@ wusa.exe /install C:\WindowsTH-KB2693643-x64.msu /nonrestart
 
 ## Confirm RSAT shows up in Programs/Features
 appwiz.cpl
+
